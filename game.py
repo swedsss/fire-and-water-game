91a9346223bfcff944114dcd48e94db32fb8c84a
Swedsss <y170806@yandex.ru>
2022-01-11 00:56:03 +0500

    Создание класса для уровня игры. Отображение стен уровня.

--- game.py
+++ game.py
@@ -1,11 +1,44 @@
+import os
 import pygame
 
+SPRITE_SIZE = 32
+MAX_LEVEL_SIZE = 25
+
 TITLE = 'Огонь и вода'
-SCREEN_WIDTH, SCREEN_HEIGHT = 800, 800
+SCREEN_WIDTH = MAX_LEVEL_SIZE * SPRITE_SIZE
+SCREEN_HEIGHT = MAX_LEVEL_SIZE * SPRITE_SIZE
 
 FPS = 60
 
 BLACK = pygame.Color('black')
+WHITE = pygame.Color('white')
+
+CURRENT_DIR = os.path.dirname(__file__)
+LEVELS_DIR = os.path.join(CURRENT_DIR, 'levels')
+
+LEVEL_ELEM_FLOOR = "."
+LEVEL_ELEM_WALL = "#"
+
+
+class Level:
+    """Уровень игры"""
+    def __init__(self, filename):
+        self.filename = filename
+        self.width, self.height = 0, 0
+        self.data = []
+        self.load_level(filename)
+
+    def load_level(self, filename):
+        """Загрузка уровня"""
+        fullname = os.path.join(LEVELS_DIR, filename)
+
+        with open(fullname) as f:
+            data = [line.strip() for line in f.readlines()]
+
+        self.width = max(map(len, data))
+        self.height = len(data)
+        data = list(map(lambda x: x.ljust(self.width, '.'), data))
+        self.data = data
 
 
 class Game:
@@ -21,9 +54,13 @@ class Game:
 
         self.all_sprites = pygame.sprite.Group()
 
+        self.level = None
+
     def new_game(self):
         """Создание новой игры"""
         self.all_sprites.empty()
+
+        self.level = Level('level1.txt')
 
     def process_events(self):
         """Обработка событий игры"""
@@ -39,6 +76,13 @@ class Game:
     def display(self):
         """Отрисовка элементов игры"""
         self.screen.fill(BLACK)
+
+        # Отображение прямоугольников на месте стен
+        for row in range(self.level.height):
+            for col in range(self.level.width):
+                if self.level.data[row][col] == LEVEL_ELEM_WALL:
+                    pygame.draw.rect(self.screen, WHITE, (col * SPRITE_SIZE, row * SPRITE_SIZE,
+                                                          SPRITE_SIZE, SPRITE_SIZE))
 
         self.all_sprites.draw(self.screen)
 
